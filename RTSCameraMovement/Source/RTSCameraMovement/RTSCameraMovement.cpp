f14a0b104fdc4b341c450569204af54a61b1820c
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2022-01-14 20:43:38 +0100

    empty project

---
+++ RTSCameraMovement/Source/RTSCameraMovement/RTSCameraMovement.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RTSCameraMovement.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RTSCameraMovement, "RTSCameraMovement" );
---
+++ RTSCameraMovement/Source/RTSCameraMovement/RTSCameraMovementGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RTSCameraMovementGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTSCAMERAMOVEMENT_API ARTSCameraMovementGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
